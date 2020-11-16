344e72400992b7aa8138028c38278ff87f39509d
Noah Razafindrabe <noah.razafindrabe@outlook.com>
2020-11-16 17:36:48 +0100

    minor update

--- dashboard/index.js
+++ dashboard/index.js
@@ -2,6 +2,24 @@ function loadcsv(data){
     return d3.csv(data);
 }
 
-const data = loadcsv('../data/Mother_Jones_Mass_Shootings_Database_1982_2020_Sheet1.csv')
+function loadjson(data){
+    return d3.json(data);
+}
 
-console.log(data)
+function filterGeo(data){
+    statesGeo = [];
+    for(var i=0; i < data.features.length; i++){
+        statesGeo.push(data.features[i].properties.name);
+    }
+    return statesGeo;
+}
+
+let USStates = '../data/us-states.json';
+
+let dataset = loadjson(USStates);
+
+let statesGeo = dataset.then(function(data){
+    return filterGeo(data);
+ });
+
+ console.log(statesGeo);
