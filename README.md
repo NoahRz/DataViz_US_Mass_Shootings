3c6a4f3fe030c8e32d01f5e7a319158e4672e953
Noah Razafindrabe <noah.razafindrabe@outlook.com>
2020-11-18 00:07:04 +0100

    added map

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DataViz_US_Mass_Shootings
+
+to start server
+```
+python -m SimpleHTTPServer (for Python 2)
+python -m http.server (for Python 3)
+```
--- dashboard/index.js
+++ dashboard/index.js
@@ -1,3 +1,6 @@
+console.log('v3', d3version3.version)
+console.log('v5', d3.version)
+
 function loadcsv(data){
     return d3.csv(data);
 }
@@ -18,8 +21,43 @@ let USStates = '../data/us-states.json';
 
 let dataset = loadjson(USStates);
 
-let statesGeo = dataset.then(function(data){
-    return filterGeo(data);
- });
 
- console.log(statesGeo);
+var w = 1400;
+var h = 700;
+
+var svg = d3.select("div#d3-US-map")
+.append("svg")
+.attr("preserveAspectRatio", "xMinYMin meet")
+.attr("viewBox", "0 0 " + w + " " + h)
+.style("background","#fff")
+.classed("svg-content", true);
+
+var projection = d3version3.geo.albersUsa().translate([w/2, h/2]).scale([1000]);
+var path = d3version3.geo.path().projection(projection);
+
+// drawmap for multiple promises
+/* Promise.all([dataset, dataset1, ...]).then(function(values){
+    // draw map
+ 
+    svg.selectAll("path")
+        .data(values[0].features)
+        .enter()
+        .append("path")
+        .attr("class","states")
+        .attr("d", path)
+}); */
+
+// drawmap for one promise
+function drawMap(data){
+    svg.selectAll("path")
+    .data(data.features)
+    .enter()
+    .append("path")
+    .attr("class","states")
+    .attr("d", path);
+
+}
+
+dataset.then((data)=>{
+    return drawMap(data);
+})
